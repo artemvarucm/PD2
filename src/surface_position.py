43722902c16cfb380d4b9d429973edd9f18fac02
jesrod06 <127731282+jesrod06@users.noreply.github.com>
2025-01-29 11:45:28 +0100

    se unificado la extracción de datos se surface position en una única función

--- src/surface_position.py
+++ src/surface_position.py
@@ -6,17 +6,15 @@ import pyModeS as pms
 TC: ENTRE 5 y 8
 TRK (Trayectoria terrestre)
 MOV (VELOCIDAD EN TIERRA)
+LATITUD
+LONGITUD
 """
-def getSurfacePosition(hex):
+def getSurfacePosition(hex, ref_lat, ref_lon):
     speed, trk, vertical_speed, tag = pms.adsb.velocity(hex)
+    lat, lon = pms.adsb.position_with_ref(hex, ref_lat, ref_lon)
+    return speed, trk, vertical_speed, tag, lat, lon
 
 
 
-"""
-Devuelve LA LATITUD Y LONGITUD DEL AVIÓN EN FUNCIÓN DEL RADAR
-"""
 
-def getSurfacePos(msg, ref_lat, ref_lon):
-    lat, lon = pms.adsb.position_with_ref(msg, ref_lat, ref_lon)
-    return lat, lon
 
