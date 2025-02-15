e1d691cafa219c58bf9e1183eabafc2b0f6002eb
Artem <artemvar@ucm.es>
2025-02-15 18:30:17 +0100

    Aviones con icono y desaparecen a cabo de un rato

--- src/visualization/mapa/dynamic_map.py
+++ src/visualization/mapa/dynamic_map.py
@@ -1,6 +1,9 @@
 import folium
 import folium.plugins
 import pandas as pd
+import math
+import base64
+
 m = folium.Map(location=[40.51, -3.53], zoom_start=12)
 
 df = pd.read_csv("data/ex2/preprocess_mapa_mini.csv")
@@ -19,6 +22,17 @@ lines = [
     for icao in df.icao.unique()
 ]
 
+with open("./assets/icons/airplane_air.svg", "r") as file:
+    svg_air_data = file.read()
+
+with open("./assets/icons/airplane_ground.svg", "r") as file:
+    svg_ground_data = file.read()
+
+def getIcon(onGround, svg_air_data, svg_ground_data):
+    svg_data = svg_ground_data if onGround else svg_air_data
+    return f"data:image/svg+xml;base64,{base64.b64encode(svg_data.encode()).decode()}"
+
+
 features = [
     {
         "type": "Feature",
@@ -28,10 +42,14 @@ features = [
         },
         "properties": {
             "times": line["dates"],
-            "icon": "plane",
+            "icon": "marker",
+            "iconstyle": {
+                "iconUrl": getIcon(False, svg_air_data, svg_ground_data),
+                "iconSize": [20, 20],
+            },
             "style": {
-                "color": line["color"],
-                "weight": line["weight"] if "weight" in line else 5,
+                "color": line["color"], # aqui podemos meter el color (velocidad)
+                "weight": 2,
             },
         },
     }
@@ -43,6 +61,7 @@ folium.plugins.TimestampedGeoJson(
         "type": "FeatureCollection",
         "features": features,
     },
+    duration="PT1H", # los datos que han estado mostrados durante una hora, se eliminan
     period="PT1M",
     auto_play=True,
     add_last_point=True,
