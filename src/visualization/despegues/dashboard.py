d9073e19cf1e1c3c0735eeace0f290558b88a130
Artem <artemvar@ucm.es>
2025-02-20 23:13:10 +0100

    path

--- src/visualization/despegues/dashboard.py
+++ src/visualization/despegues/dashboard.py
@@ -11,7 +11,7 @@ import plotly.express as px
 import matplotlib
 matplotlib.use('Agg')
 
-df = pd.read_csv('../../../data/ex1/eventos_espera_semana_nuevo.csv')
+df = pd.read_csv('data/ex1/eventos_espera_semana_nuevo.csv')
 
 df['ultimo_parado'] = pd.to_datetime(df['ultimo_parado'])
 df['despegue'] = pd.to_datetime(df['despegue'])
